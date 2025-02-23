54132f825d86b64b34cc5d5babd20ae87fde9ce6
Bulamanki123 <142177172+Bulamanki123@users.noreply.github.com>
2025-02-23 18:53:04 +0300

    Add project files.

---
+++ classintro2/Program.cs
@@ -0,0 +1,36 @@
+﻿// clas kullanımı bu yazıya sonradan bak
+Kurs kurs1 = new Kurs(); // clasa bir değişken olusturmak için 2
+kurs1.KursAdi = "C#"; /// degiskenlere özellik değeri karsılığı atamak için bu
+kurs1.egitmen = "Engin Demiroğ";
+kurs1.izlemeOrani = 68;
+//artık kurs1 in var ve tipi Kurs
+Kurs kurs2=new Kurs();
+kurs2.KursAdi = "Javascript";
+kurs2.egitmen = "Sena Subaş";
+kurs2.izlemeOrani = 100;
+Kurs kurs3 = new Kurs();
+kurs3.KursAdi = "css";
+kurs3.egitmen = "Kerem Bilginer";
+kurs3.izlemeOrani = 70;
+// ekrana yazdirmasi böyle STATİK OLRAK
+//Console.WriteLine(kurs2.KursAdi + " : " + kurs2.egitmen);
+//Console.WriteLine(kurs3.KursAdi + ":" + kurs3.egitmen);
+
+Kurs[] kurslar = new Kurs[] {kurs1,kurs2,kurs3 };
+         //tam alttaki veri tipi kurs 
+foreach (Kurs kurs in kurslar)
+{
+    Console.WriteLine(kurs.KursAdi + ":" + kurs.egitmen + ":" + kurs.izlemeOrani);
+}
+
+public class Kurs  // bir clas olusturmak için bu 1
+{
+    public string KursAdi { get; set; }
+    public string egitmen { get; set; }
+    public int izlemeOrani { get; set; }
+   
+}
+// kurs bir değişken TÜRÜDÜR string bir değişken türüyse kurs da bir değişken türüdür. kendi veri tipini oluşturuyoomuş gibi düşün
+// propa intelliesence yapınca  public int MyProperty { get; set; } bunu gosteriyor property ozellik demek biz bir clas oluşturduk ismi kurs.
+// bu classın özelliklerini propla oluşturuyoruz
+
---
+++ deneme1/Program.cs
@@ -0,0 +1,63 @@
+﻿//dongulerin tekrari
+// donguleri c# birbirine benzeyen işlermleri tekrar etmek için kullanırız
+// birer birier arttırmak için i++ ikişker arttırmak için i=i+2
+
+
+// dizi array örneği yapalım
+// mesela bir kurstaki başlıklı elemanlar yazılırken onları sıralamak için tek bir string değil birden fazla stringi bir arada tutmak için
+// degiskenin türünün string olmasını istiyorsan string değil string[] yani string ve array olması lazım array=[]
+// tek bir veri tutulacaksa string=sfefas birden fazlaysa string[]
+string[] kurslar = new string[] {"java","pyhton","c","angular","html","css" };
+
+
+for (int i = 0; i <kurslar.Length; i++)
+{
+    Console.WriteLine(kurslar[i]);
+}
+Console.WriteLine("sayfa sonuna ulastiniz");
+
+
+
+
+
+
+string[] cookies = new string[] {"white chocholate","mnm","dark chocholate","milk chocholate","not sugar" };
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine(cookies[i]);
+}
+
+string[] dizi = new string[] {"aski memnu","k,raz mevsimi","poyraz karayel" };
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(dizi[i]);
+}
+
+
+// foreach dizi temelli yapıları tek tek dönmeye yarar. dizilere uygulanır
+//alttaki kurslar = kursları dolaş. kurslardan kastım yukarıdaki olusturduğum ve elemanları olan kurslar
+// ininci eleman yazmaya gerek yok herbirkus takma eleman
+foreach (string herbirkurs in kurslar)
+{
+    Console.WriteLine(herbirkurs); 
+}
+
+foreach (var kurabiye in cookies)
+{
+    Console.WriteLine(kurabiye);
+}
+
+foreach (var kurss in kurslar)
+{
+    Console.WriteLine(kurss);
+}
+
+
+
+
+string[] Mobilya = new string[] {"baza","baza başlığı","komodin","makyaj masası","gardırop","oturma pufu" };
+foreach (string esyaismi in Mobilya)
+{
+    Console.WriteLine(esyaismi);
+}
+Console.WriteLine("ürünlerimiz listede gördündüğü kadardır.");
